24aafbafc35d56704431f5b46b4d459de8bb7be6
JhonatanMattana <kaverinhaa@hotmail.com>
2017-10-13 16:05:36 -0300

    criando component manualmente

---
+++ AulasAngular2-4/Aula03 - Hello world/primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'meu-primeiro-component', //vai ser o nome da tag html que nós utilizaremos depois.
+  template: `
+    <p>Meu primeiro component com Angular 2!</p>
+  `
+})
+ export class MeuPrimeiroComponent { }
